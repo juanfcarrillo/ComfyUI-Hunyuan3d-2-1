dffd6cd67a52b9aded64df1464fa4a3245208404
juanfcarrillo <juanfi444@outlook.com>
2025-09-06 14:08:28 -0500

    fix(custom_rasterizer): packed correct wheel

--- runner-scripts/compile-texture-modules.sh
+++ runner-scripts/compile-texture-modules.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Compile custom rasterizer
-pip install wheel/*.whl
+pip install hy3dpaint/custom_rasterizer/dist/custom_rasterizer-0.1-cp311-cp311-linux_x86_64.whl
 
 # Compile differentiable renderer
 cd hy3dpaint/DifferentiableRenderer
